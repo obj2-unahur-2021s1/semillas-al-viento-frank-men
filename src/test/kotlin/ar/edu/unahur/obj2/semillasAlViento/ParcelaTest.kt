2a67a462073b5c1d43e47b5591d9d3c2704342ef
Daniel Mendez <rdmmendez@hotmail.com>
2021-06-04 20:44:36 -0300

    algunos arreglos
    Signed-off-by: Daniel Mendez <rdmmendez@hotmail.com>

--- src/test/kotlin/ar/edu/unahur/obj2/semillasAlViento/ParcelaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/semillasAlViento/ParcelaTest.kt
@@ -1,6 +1,8 @@
 package ar.edu.unahur.obj2.semillasAlViento
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
@@ -19,33 +21,55 @@ class ParcelaTest: DescribeSpec({
             }
         }
         describe("De una parcela se debe conocer:"){
-            it("Se debe conocer la superficie:"){
+            it("La superficie:"){
                 val parcelaDeMarcela = Parcela(15,20,12)
                 parcelaDeMarcela.superficie().shouldBe(300) }
-            describe("Se debe conocer la cantidad de plantas que tolera:"){
-                it("Largo mayor que ancho."){
-                    val parcelaLarga = Parcela(5,6,12)
-                    // 30 / 3 + 6
-                    parcelaLarga.cantidadMaximaPlantas().shouldBe(16)
-                }
-                it("Ancho mayor que largo."){
-                    val parcelaAncha = Parcela(6,5,12)
-                    // 30 / 5
-                    parcelaAncha.cantidadMaximaPlantas().shouldBe(6)
-                }
+
+            it("Cantidad Maxima deplantas"){
+                val parcelaLarga = Parcela(5,6,12)
+                // 30 / 3 + 6
+                parcelaLarga.cantidadMaximaPlantas().shouldBe(16)
+                val parcelaAncha = Parcela(6,5,12)
+                // 30 / 5
+                parcelaAncha.cantidadMaximaPlantas().shouldBe(6)
             }
-            it("Se debe saber si la parcela tiene complicaciones:"){
-                val parcelaDeMarcela = Parcela(15,20,12)
-                /*ERROR DE PLANTA: no estoy pudiendo asignarle ningun valor a la
-                * altura. deberia ser un int, a lo sumo un double. */
-                //val plantita = Menta(2000,0.5)
+
+            it("Si tiene complicaciones:"){
+                val parcelaDeMarcela = Parcela(15,20,6)
+                val plantita = Menta(2000,0.5F)
+                parcelaDeMarcela.plantar(plantita)
+                plantita.parcelaTieneComplicaciones(parcelaDeMarcela).shouldBeTrue()
             }
-            it("Se debe poder plantar una planta:"){
-                val parcelaDeMarcela = Parcela(15,20,12)
-                /*Mismo error de planta: Altura es un float.*/
-                //val plantitaDeSoja = Soja(2000,0.7,false)
+            it("Si se puede plantar:"){
+                val parcelaDeMarcela = Parcela(15,20,9)
+                val plantitaDeSoja = Soja(2000,0.7F,false)
+                val menta = Menta(2010,0.3F)
+                parcelaDeMarcela.plantar(plantitaDeSoja)
+                parcelaDeMarcela.plantar(menta)// no se captura el error
+                parcelaDeMarcela.plantas.size.shouldBe(1)
             }
         }
     }
-
+    describe("Agricultoras"){
+        it("parcelas semilleras"){
+            val parcela = Parcela(5,5,6)
+            val listParcel = mutableListOf<Parcela>()
+            val menta = Menta(2017,0.5F)
+            parcela.plantar(menta)
+            listParcel.add(parcela)
+            val agricultora = Agricultora(listParcel)
+            agricultora.parcelasSemilleras().shouldContainExactly(listParcel)
+        }
+        it("plantar estratégicamente"){
+            val parcela = Parcela(5,5,6)
+            val parcela1 = Parcela(1,2,6)
+            val listParcel = mutableListOf<Parcela>()
+            val menta = Menta(2017,0.5F)
+            listParcel.add(parcela)
+            listParcel.add(parcela1)
+            val agricultora = Agricultora(listParcel)
+            agricultora.plantarEstrategicamente(menta)
+            parcela.plantas.size.shouldBe(1)
+        }
+    }
 })
